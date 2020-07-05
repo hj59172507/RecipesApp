22179d6898baa35fba88880eac4e420763893f89
huajian lin <huaj59172507@gmail.com>
2020-07-04 23:15:36 -0400

    change toggle to listen for document click, so that click away also works

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,7 +1,8 @@
 import {
   Directive,
-  HostListener,
-  HostBinding
+  HostListener,
+  HostBinding,
+  ElementRef
 } from '@angular/core'
 
 @Directive({
@@ -10,7 +11,8 @@ import {
 export class DropdownDirective {
   @HostBinding('class.open') isOpen = false;
 
-  @HostListener('click') toggleOpen() {
-    this.isOpen = !this.isOpen;
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
   }
+  constructor(private elRef: ElementRef) { }
 }
